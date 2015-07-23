85144789dd7aacd336a2eee6acbbfcaf678e8ac3
David Nickerson <nickerso@users.sourceforge.net>
2015-07-23 22:37:18 +1200

    Doing some testing on provided argument values. Now require that the working folder is a valid folder on the file system so that we can write to it.

--- src/gms.cpp
+++ src/gms.cpp
@@ -16,12 +16,23 @@ limitations under the License.Some license of other
 
 #include "version.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <iostream>
 #include <curl/curl.h>
 
 #include "gms_config.h"
 #include "serverdata.h"
 #include "libhttpd-utils.h"
+
+// modified slightly from http://stackoverflow.com/questions/18100097/portable-way-to-check-if-directory-exists-windows-linux-c
+static bool dirExists(const char *path)
+{
+    struct stat info;
+    if(stat(path, &info) != 0) return false;
+    if(info.st_mode & S_IFDIR) return true;
+    return false;
+}
 
 static void printInformation()
 {
@@ -34,7 +45,7 @@ static void printUsage(const char* progName)
     std::cerr << "\nUsage: " << progName << " <port> <working folder> [PMR2 instance]\n"
               << "\n  port - (required) the port number GMS should listen on, e.g., 1234.\n"
               << "\n  working folder - (required) the full path to the local folder in which\n"
-              << "    to store working copies of repository workspaces.\n"
+              << "    to store working copies of repository workspaces and other information (must be a path to a valid folder).\n"
               << "\n  PMR2 instance - (optional) the instance of PMR2 to use as the repository.\n"
               << "    Will not override the existing origin in any working copies already\n"
               << "    in the <working folder>. Available options are:\n"
@@ -52,7 +63,20 @@ int main(int argc, char *argv[])
         printUsage(argv[0]);
         return -1;
     }
+    // check arguments
+    int portNumber = atoi(argv[1]);
+    if (portNumber < 1000)
+    {
+        std::cerr << "WARNING: port numbers less that 1000 might require administrator "
+                  << "access on some platforms." << std::endl;
+    }
     std::string workingFolder(argv[2]);
+    if (!dirExists(workingFolder.c_str()))
+    {
+        std::cerr << "Working folder: " << workingFolder << "; doesn't seem to be a valid folder." << std::endl;
+        printUsage(argv[0]);
+        return -3;
+    }
     std::string repositoryUrl = "http://staging.physiomeproject.org";
     if (argc > 3)
     {
